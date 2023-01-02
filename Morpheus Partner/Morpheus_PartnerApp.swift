1bd1d096cda07aed5fed3e2b098f536a7e6de0ce
José Ruiz <ruiz211@duck.com>
2023-01-02 16:10:55 -0500

    Initial commit

---
+++ Morpheus Partner/Morpheus_PartnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morpheus_PartnerApp.swift
+//  Morpheus Partner
+//
+//  Created by José Ruiz on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Morpheus_PartnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
